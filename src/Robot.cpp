9b82d37795d27e4598a7cf79df3ed14408e9928a
Fabien Pinel <pinel.fabien@gmail.com>
2014-11-18 14:31:10 +0100

    Début implémentation Robot

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,1 +1,57 @@
+
+using namespace std;
 #include "Robot.h"
+
+
+/* Méthodes du robot */
+void afficher(){
+	//instancier l'afficheur
+	//Afficheur aff();
+}
+void avancer(){
+
+}       
+void figer(){
+
+}
+void poser(){
+
+}
+void rencontrerPlot(Plot p){
+
+}
+void repartir(){
+
+}
+void saisir(Objet o){
+
+}
+void tourner(){
+
+}
+
+
+/* Fonctions du robot */
+int peser(){
+	return 0;
+}
+int evaluerPlot(){
+	return 0;
+}
+
+/* Setters */
+void etat_courant(EtatRobot* e){
+
+}
+void plot(Plot p){
+
+}
+void objet(Objet o){
+
+}
+void direction(int d){
+
+}
+void position(Position p){
+	
+}
